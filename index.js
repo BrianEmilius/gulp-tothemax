7be5dc7e1f61bf2a350498d17cd593bdb6f44aed
Brian Emilius <brian@brianemilius.com>
2020-01-10 12:09:17 +0100

    make cli work

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var fs = require("fs");
 var path = require("path");
 var mkdir = require("mkdirp");
-var { execSync } = require("child_process");
+var { exec } = require("child_process");
 
 var TEMPLATE_DIR = path.join(__dirname, "pkg");
 var MODE_0666 = parseInt('0755', 8);
@@ -45,24 +45,13 @@ copyFileMulti("src/images", "./src/images");
 copyFileMulti("src/media", "./src/media");
 copyFile("gulpfile.js", "./gulpfile.js");
 
-/* var package = JSON.parse(require("./package"));
+exec("npm i -D @babel/core @babel/preset-env gulp gulp-babel gulp-clean-css gulp-concat gulp-connect gulp-imagemin gulp-pug gulp-rename gulp-sass gulp-sourcemaps gulp-uglify imagemin-jpeg-recompress", function(err, stdout, stderr) {
+	if (err) {
+		console.log(err);
+		process.exit(1);
+	}
 
-package.devDependencies = {
-	"@babel/core": "^7.7.7",
-	"@babel/preset-env": "^7.7.7",
-	"gulp": "^4.0.2",
-	"gulp-babel": "^8.0.0",
-	"gulp-clean-css": "^4.2.0",
-	"gulp-concat": "^2.6.1",
-	"gulp-connect": "^5.7.0",
-	"gulp-imagemin": "^6.2.0",
-	"gulp-pug": "^4.0.1",
-	"gulp-rename": "^2.0.0",
-	"gulp-sass": "^4.0.2",
-	"gulp-sourcemaps": "^2.6.5",
-	"gulp-uglify": "^3.0.2",
-	"imagemin-jpeg-recompress": "^6.0.0"
-};
-
-write("./package.json", JSON.stringify(package)); */
-
+	stdout.on("data", function(data) {
+		console.log(data.toString());
+	});
+});
